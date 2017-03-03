38b729db1167d48c9821d2af362cf8addbe1b0ed
Garrett Schroath <gtschroath@gmail.com>
2017-03-02 20:17:13 -0600

    fixed file structure and refs

---
+++ public/js/fbconfig.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAn1vicyN6KBjZyN0iCCTOUeCfjQ9GgYFw",
+    authDomain: "teaching-with-sound.firebaseapp.com",
+    databaseURL: "https://teaching-with-sound.firebaseio.com",
+    storageBucket: "teaching-with-sound.appspot.com",
+    messagingSenderId: "1049752919817"
+  };
+  firebase.initializeApp(config);
+
