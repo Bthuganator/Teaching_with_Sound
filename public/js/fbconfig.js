411cab24f165e38f8617f01c78c42cd8418a7913
Bthuganator <Bretmarkowski@gmail.com>
2017-03-07 20:25:53 -0600

    Updated Auth URL

--- public/js/fbconfig.js
+++ public/js/fbconfig.js
@@ -1,7 +1,8 @@
   // Initialize Firebase
   const config = {
     apiKey: "AIzaSyAn1vicyN6KBjZyN0iCCTOUeCfjQ9GgYFw",
-    authDomain: "teaching-with-sound.firebaseapp.com",
+    //authDomain: "teaching-with-sound.firebaseapp.com",
+	authDomain: "teachingwithsound.com",
     databaseURL: "https://teaching-with-sound.firebaseio.com",
     storageBucket: "teaching-with-sound.appspot.com",
     messagingSenderId: "1049752919817"
