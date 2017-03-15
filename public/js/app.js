e7053ecf0621d17fcc711c202e67d27fae7784cf
Bthuganator <Bretmarkowski@gmail.com>
2017-03-14 20:13:36 -0500

    Updated Button to remove email address

--- public/js/app.js
+++ public/js/app.js
@@ -43,34 +43,23 @@ var db = firebase.database().ref();
                         <h2 class="section-heading" v-html="a.heading"></h2>
                         <p class="lead" v-html="a.body"></p>
                         <div v-if="index === 3">
-                            <form class="paypalForm" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                            <input type="hidden" name="cmd" value="_xclick">
-                            <input type="hidden" name="business" value="bretmarkowski@gmail.com">
-                            <input type="hidden" name="lc" value="US">
-                            <input type="hidden" name="item_name" value="Support Teaching with Sound">
-                            <input type="hidden" name="item_number" value="TWS1">
-                            <input type="hidden" name="button_subtype" value="services">
-                            <input type="hidden" name="no_note" value="0">
-                            <input type="hidden" name="currency_code" value="USD">
-                            <input type="hidden" name="tax_rate" value="0.000">
-                            <input type="hidden" name="shipping" value="0.00">
-                            <input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynow_LG.gif:NonHostedGuest">
-                            <table>
-                            <tr><td><input type="hidden" name="on0" value="Teaching with Sound Support"></td></tr><tr><td>
-                            <select class="form-control" name="os0">
-                                <option value="Helpful site">Helpful site $1.00 USD</option>
-                                <option value="I want more">I want more $3.00 USD</option>
-                                <option value="Make it great">Make it great $5.00 USD</option>
-                            </select> </td></tr>
-                            </table>
-                            <input type="hidden" name="currency_code" value="USD">
-                            <input type="hidden" name="option_select0" value="Helpful site">
-                            <input type="hidden" name="option_amount0" value="1.00">
-                            <input type="hidden" name="option_select1" value="I want more">
-                            <input type="hidden" name="option_amount1" value="3.00">
-                            <input type="hidden" name="option_select2" value="Make it great">
-                            <input type="hidden" name="option_amount2" value="5.00">
-                            <input type="hidden" name="option_index" value="0"> 
+						<form class="paypalForm" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                            
+							<input type="hidden" name="cmd" value="_s-xclick">
+							<input type="hidden" name="hosted_button_id" value="8SFGBCDD59YQN">
+							<table>
+							<tr>
+								<td>
+									<input type="hidden" name="on0" value="Teaching with Sound Support">Teaching with Sound Support</td></tr><tr><td>
+										<select name="os0">
+											<option value="Helpful site">Helpful site $1.00 USD</option>
+											<option value="I want more">I want more $3.00 USD</option>
+											<option value="Make it great">Make it great $5.00 USD</option>
+											</select> 
+								</td>
+							</tr>
+							</table>
+							<input type="hidden" name="currency_code" value="USD">
                             <p>                       
                                 <button class="btn btn-default btn-g" name="submit" alt="PayPal - The safer, easier way to pay online!">Support Now</button>
                             </p>
