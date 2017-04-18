0aa05d85836dd05feba9d87edaab00d022778748
Garrett Schroath <gtschroath@gmail.com>
2017-04-17 20:42:20 -0500

    added audio player

--- src/components/sound-button.vue
+++ src/components/sound-button.vue
@@ -2,8 +2,6 @@
     <button class='btn btn-primary play-sound vue-grid-item-content' v-on:click='playSound(this)'>
         <i class="fa fa-3x" v-bind:class="snd.icon"></i>
         <i class="fa fa-3x" v-bind:class="snd.icon2"></i>
-        <!--<span class='glyphicon glyphicon-play'></span> <span class='display-name'>{{snd.display_name}}</span>-->
-        <!--<progress class='sound-progress' value='0' max='100'></progress>-->
         <audio class='hidden' :src='snd.sound_url' :type='snd.audio_type' autostart='false' codecs='mp3'></audio>
     </button>
 </template>
@@ -41,21 +39,11 @@ export default {
       } else {
         newSound = $(el).children('audio')[0]
         newSound.play()
-        // newSound.addEventListener('loadeddata', function () {
-        //   console.log('Audio data loaded')
-        //   console.log('Audio duration: ' + this.duration)
-        // })
         $(el).removeClass('btn-primary').addClass('btn-success')
         $(newSound).on('ended', function () {
           $(el).removeClass('btn-success').addClass('btn-primary')
           $($(el).find('.sound-progress')[0]).attr('value', 0)
         })
-        // $(newSound).on('timeupdate', function () {
-        //   if (newSound.currentTime && newSound.duration) {
-        //     var w = 100 * (newSound.currentTime / newSound.duration)
-        //     $(el).children('.sound-progress').attr('value', w)
-        //   }
-        // })
       }
       this.$emit('click', newSound)
     }
