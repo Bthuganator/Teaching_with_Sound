ad4f95bfd9a30d85575fcb9d037412f246449789
Garrett Schroath <gtschroath@gmail.com>
2017-04-17 19:49:36 -0500

    icon only buttons

--- src/components/sound-button.vue
+++ src/components/sound-button.vue
@@ -1,7 +1,8 @@
 <template>
     <button class='btn btn-primary play-sound vue-grid-item-content' v-on:click='playSound(this)'>
-        <span class='glyphicon glyphicon-play'></span> <span class='display-name'>{{snd.display_name}}</span>
-        <progress class='sound-progress' value='0' max='100'></progress>
+        <i class="fa fa-3x" v-bind:class="snd.icon"></i>
+        <!--<span class='glyphicon glyphicon-play'></span> <span class='display-name'>{{snd.display_name}}</span>-->
+        <!--<progress class='sound-progress' value='0' max='100'></progress>-->
         <audio class='hidden' :src='snd.sound_url' :type='snd.audio_type' autostart='false' codecs='mp3'></audio>
     </button>
 </template>
@@ -25,39 +26,35 @@ export default {
       this.$emit('created', this)
       var newSound
       var el = this.$el
-      $(el).parents('.sound-well').siblings().find('button .glyphicon').removeClass('glyphicon-stop').addClass('glyphicon-play')
       $(el).parents('.sound-well').siblings().find('button').removeClass('btn-success').addClass('btn-primary')
       if (this.currentSound !== null) {
         this.currentSound.pause()
         this.currentSound.currentTime = 0
         $(this.currentSound).siblings('.sound-progress').attr('value', 0)
       }
-      if (this.currentSound && $(el).children('.glyphicon').hasClass('glyphicon-stop')) {
+      if (this.currentSound && $(el).hasClass('btn-success')) {
         this.currentSound.pause()
         this.currentSound.currentTime = 0
         newSound = this.currentSound
-        $(el).children('.glyphicon').removeClass('glyphicon-stop').addClass('glyphicon-play')
         $(el).removeClass('btn-success').addClass('btn-primary')
       } else {
         newSound = $(el).children('audio')[0]
         newSound.play()
-        newSound.addEventListener('loadeddata', function () {
-          console.log('Audio data loaded')
-          console.log('Audio duration: ' + this.duration)
-        })
-        $(el).children('.glyphicon').removeClass('glyphicon-play').addClass('glyphicon-stop')
+        // newSound.addEventListener('loadeddata', function () {
+        //   console.log('Audio data loaded')
+        //   console.log('Audio duration: ' + this.duration)
+        // })
         $(el).removeClass('btn-primary').addClass('btn-success')
         $(newSound).on('ended', function () {
-          $(el).children('.glyphicon').removeClass('glyphicon-stop').addClass('glyphicon-play')
           $(el).removeClass('btn-success').addClass('btn-primary')
           $($(el).find('.sound-progress')[0]).attr('value', 0)
         })
-        $(newSound).on('timeupdate', function () {
-          if (newSound.currentTime && newSound.duration) {
-            var w = 100 * (newSound.currentTime / newSound.duration)
-            $(el).children('.sound-progress').attr('value', w)
-          }
-        })
+        // $(newSound).on('timeupdate', function () {
+        //   if (newSound.currentTime && newSound.duration) {
+        //     var w = 100 * (newSound.currentTime / newSound.duration)
+        //     $(el).children('.sound-progress').attr('value', w)
+        //   }
+        // })
       }
       this.$emit('click', newSound)
     }
