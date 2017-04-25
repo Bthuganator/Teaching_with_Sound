efd8fdd5f858d7856c9cfc50483780a395df0357
Garrett Schroath <gtschroath@gmail.com>
2017-04-24 21:41:38 -0500

    setting up for crud

--- src/views/LessonBoard.vue
+++ src/views/LessonBoard.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="container">                            
+  <div>
+    <div class="checkbox">
+      <label><input type="checkbox" v-model="fullWidth" checked>Full Width</label>
+    </div>
+    <div v-model="fullWidth" v-bind:class="fullWidth ? '' : 'container'">                            
         <lesson-section :boxes='boxes'></lesson-section>                            
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,15 +18,30 @@ export default {
   components: {
     LessonSection
   },
+  data: function () {
+    return {
+      fullWidth: false
+    }
+  },
   computed: {
     ...mapGetters({
       db: 'db'
     })
   },
   firebase: function () {
-    console.log('test')
     return {
       boxes: this.db.ref().child('soundstest').orderByChild('i')
+    }
+  },
+  methods: {
+    saveRecord (box) {
+      this.$firebaseRefs.boxes.child(box['.key']).set(box)
+    },
+    addRecord (box) {
+      this.$firebaseRefs.boxes.push(box)
+    },
+    removeRecord (box) {
+      this.$firebaseRefs.boxes.child(box['.key']).remove()
     }
   }
 }
